d6f7c7cdb1d3dd3cd19af509e363ecd6f9c92f8e
Alexis Enston <xn@outlook.com>
2017-03-17 11:34:44 +0000

    cleanup code

--- sketches/sensors_serial/sensors_serial.ino
+++ sketches/sensors_serial/sensors_serial.ino
@@ -1,32 +1,19 @@
 /*
-  Analog input, analog output, serial output
- 
- Reads an analog input pin, maps the result to a range from 0 to 255
- and uses the result to set the pulsewidth modulation (PWM) of an output pin.
- Also prints the results to the serial monitor.
- 
- The circuit:
- * potentiometer connected to analog pin 0.
-   Center pin of the potentiometer goes to the analog pin.
-   side pins of the potentiometer go to +3.3V and ground
- * LED connected from digital pin 9 to ground
- 
- created 29 Dec. 2008
- modified 30 Aug 2011
- by Tom Igoe
- 
- This example code is in the public domain.
- 
+Room Availibility Sensor
+Alexis Enston, Matt Bell
+
+Uses microphone and PIR to detect people in a room
+
  */
 
 // These constants won't change.  They're used to give names
 // to the pins used:
-const int analogInPin = 6;  // Analog input pin that the mic is attached to
+const int micPin = 6;  // Analog input pin that the mic is attached to
 const int pirPin = 5;
+const int micSampleTime = 50; //ms
 
 
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value output to the PWM (analog out)
+
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -36,17 +23,16 @@ void setup() {
  
 }
 
-void loop() {
-  
-  
-  //microphone
+int getMicValue() {
+    //microphone
   //find min and max peaks
+  unsigned int sensorValue = 0;
   unsigned int signalMin = 4096;
   unsigned int signalMax = 0;
   unsigned long startM = millis();
-  while( millis() < startM + 50) {
+  while( millis() < startM + micSampleTime) {
     // read the analog in value:
-    sensorValue = analogRead(analogInPin); 
+    sensorValue = analogRead(micPin); 
     if (sensorValue < signalMin) {
      signalMin = sensorValue; 
     }
@@ -54,9 +40,20 @@ void loop() {
       signalMax = sensorValue;
     }
   }
+  return signalMax - signalMin;
+}
+
+int getPIR() {
+  return digitalRead(pirPin);
+}
+
+void loop() {
+   
+  //mic
+  int micValue = getMicValue();
   
   //PIR
-  int pirState = digitalRead(pirPin);
+  int pirState = getPIR();
   if(pirState)
     digitalWrite(RED_LED, HIGH);
   else
@@ -66,15 +63,8 @@ void loop() {
   // print the results to the serial monitor:
   Serial.print("PIR = ");
   Serial.print(pirState);
-  Serial.print(" mic = " );                       
-  //Serial.print(sensorValue);     
-  //Serial.print(" p2p = ");
-  Serial.println(signalMax - signalMin); 
-  //Serial.print("\t output = ");      
-  //Serial.println(outputValue);   
-
-  // wait 10 milliseconds before the next loop
-  // for the analog-to-digital converter to settle
-  // after the last reading:
+  Serial.print(" mic = " );       
+  Serial.println(micValue); 
+  
   //delay(10);                     
 }
